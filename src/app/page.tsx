702dd054f3cd35ec06caa469a4b7b1aeb7696602
Jaredgg4 <jg70456898@gmail.com>
2024-04-23 14:02:52 -0600

    added the new home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,91 +4,61 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <header className={styles.head}>
+        <h1>EPLabsMo</h1>
+        <p>Help us EpLabs pay their instructors and apprentices</p>
+      </header>
+      <div className={styles.tablediv}>
+        <table className={styles.tabl}>
+          <tr className={styles.header}>
+            <th colSpan={4}><h1>The Table</h1></th>
+          </tr>
+          <tr>
+            <td></td>
+            <td className={styles.cells}>1</td>
+            <td className={styles.cells1}>2</td>
+            <td className={styles.cells2}>3</td>
+          </tr>
+          <tr>
+          <td></td>
+            <td className={styles.cells3}>4</td>
+            <td className={styles.cells4}>5</td>
+            <td className={styles.cells5}>6</td>
+          </tr>
+          <tr>
+          <td></td>
+            <td className={styles.cells6}>7</td>
+            <td className={styles.cells7}>8</td>
+            <td className={styles.cells8}>9</td>
+          </tr>
+          <tr>
+          <td></td>
+            <td className={styles.cells9}>10</td>
+            <td className={styles.cells10}>11</td>
+            <td className={styles.cells11}>12</td>
+          </tr>
+          <tr>
+          <td></td>
+            <td className={styles.cells12}>13</td>
+            <td className={styles.cells13}>14</td>
+            <td className={styles.cells14}>15</td>
+          </tr>
+          <tr>
+          <td></td>
+            <td className={styles.cells15}>16</td>
+            <td className={styles.cells16}>18</td>
+            <td className={styles.cells17}>19</td>
+          </tr>
+        </table>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.divForm}>
+        <form className={styles.form}>
+          Username: <input type="text" name="username" id="username"></input>
+          <br></br>
+          Money: <input type="number" name="currency"></input>
+          <br></br>
+          <input type="submit" value="Donate" className={styles.btn}></input>
+        </form>
       </div>
     </main>
   );
